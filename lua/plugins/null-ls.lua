209c1c294f7836bbc596d2d4570a501e1052ac98
Duy Khanh <dzerok2020@gmail.com>
2023-03-04 00:12:43 +0700

    config tailwindcss

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,18 +1,16 @@
 return {
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = { "mason.nvim" },
-		opts = function()
-			local nls = require("null-ls")
-			return {
-				sources = {
-					nls.builtins.formatting.prettierd,
-					nls.builtins.diagnostics.eslint_d.with({
-						diagnostics_format = "[eslint] #{m}\n(#{c})",
-					}),
-				},
-			}
-		end,
-	},
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.formatting.prettierd,
+          nls.builtins.diagnostics.eslint_d.with({
+            diagnostics_format = "[eslint] #{m}\n(#{c})",
+          }),
+        },
+      }
+    end,
+  },
 }
