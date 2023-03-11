3f301c000cff7c31b4e6cdf90e0fb6028ec5b6e0
Duy Khanh <dzerok2020@gmail.com>
2023-03-11 14:18:45 +0700

    fix chatgpt

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -1,6 +1,7 @@
 return {
   {
     "jackMort/ChatGPT.nvim",
+    commit = '8820b99c',
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
     lazy = true,
     event = "VeryLazy",
