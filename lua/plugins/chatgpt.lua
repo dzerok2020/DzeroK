36665ec0064962ccb5a608f8c13d791ecda07d66
Duy Khanh <dzerok2020@gmail.com>
2023-03-12 11:27:02 +0700

    fix chatgpt

--- lua/plugins/chatgpt.lua
+++ lua/plugins/chatgpt.lua
@@ -1,12 +1,16 @@
 return {
-  {
-    "jackMort/ChatGPT.nvim",
-    commit = '8820b99c',
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-    lazy = true,
-    event = "VeryLazy",
-    opts = function()
-      require("chatgpt").setup({})
-    end
-  }
+	{
+		"jackMort/ChatGPT.nvim",
+		-- commit = '8820b99c',
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+		lazy = true,
+		event = "VeryLazy",
+		config = function()
+			require("chatgpt").setup({
+				keymaps = {
+					submit = "<C-e>",
+				},
+			})
+		end,
+	},
 }
